c2596c3efffe24c618825d6579ccf3c0c30e8274
plainionist <mail@plainionist.net>
2024-02-01 18:34:18 +0100

    Basic workflow implemented

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -1,9 +1,42 @@
-<script setup>
-
-</script>
-
 <template>
-  <main>
-    coming soon
+  <main style="font-size: larger">
+    <div><label>Translate:</label> {{ word }}</div>
+    
+    <form @submit.prevent="submit">
+      <input type="text" v-model="input" />
+      <button style="margin-left: 20px" @click="submit">Submit</button>
+    </form>
+
+    <div style="margin-top: 50px">
+      <img src="../assets/checkmark.png" v-if="answerCorrect === true" />
+      <img src="../assets/red-cross.png" v-if="answerCorrect === false" />
+    </div>
   </main>
 </template>
+
+<script>
+export default {
+  name: 'HomeView',
+  data() {
+    return {
+      dictionary: [
+        ['Haus', 'house'],
+        ['schwarz', 'black'],
+        ['schnell', 'fast']
+      ],
+      input: null,
+      answerCorrect: null
+    }
+  },
+  computed: {
+    word() {
+      return this.dictionary[0][0]
+    }
+  },
+  methods: {
+    submit() {
+      this.answerCorrect = this.dictionary[0][1] == this.input
+    }
+  }
+}
+</script>
