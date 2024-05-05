07b8f1c89d60ab156f380feb4966aa425d5748d4
plainionist <mail@plainionist.net>
2024-05-05 08:37:18 +0200

    AppView cleanup

--- web/ToDo.md
+++ web/ToDo.md
@@ -1,4 +1,3 @@
 
-- update packages
 - choose name and then dictionary
 - save result per day with every submission
---
+++ web/src/views/MainView.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h1 style="text-align: center">Welcome to Vocabu-Larry ;-)</h1>
+    <br />
+    <label style="font-weight: bold">Dictionary: </label>
+    <select v-model="dictionaryFile">
+      <option v-for="item in dictionaryFiles" v-bind:key="item.name" :value="item">{{ item.name }}</option>
+    </select>
+    <br /><br />
+    <home-view :dictionary-file="dictionaryFile" />
+  </div>
+</template>
+
+<script>
+import HomeView from './HomeView.vue'
+import frenchDictionary from '../../../dictionaries/french.txt?raw'
+import englishDictionary from '../../../dictionaries/english.txt?raw'
+import loonyEnglishDictionary from '../../../dictionaries/loony-english.txt?raw'
+
+export default {
+  name: 'MainView',
+  components: { HomeView },
+  data() {
+    return {
+      dictionaryFile: null,
+      dictionaryFiles: [
+        {
+          name: 'french',
+          content: frenchDictionary
+        },
+        {
+          name: 'english',
+          content: englishDictionary
+        },
+        {
+          name: 'loony english',
+          content: loonyEnglishDictionary
+        }
+      ]
+    }
+  },
+  created() {
+    this.dictionaryFile = this.dictionaryFiles[0]
+  }
+}
+</script>
+
