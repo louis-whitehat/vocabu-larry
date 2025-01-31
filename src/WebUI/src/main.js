7bea7b1ae268dd9f298a273cc35b53a5b29df8bf
plainionist <mail@plainionist.net>
2025-01-31 21:14:03 +0100

    simplified and migrated to pico css

--- src/WebUI/src/main.js
+++ src/WebUI/src/main.js
@@ -1,12 +1,30 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
-import router from './router.js'
-import 'materialize-css/dist/css/materialize.min.css'
-import 'material-design-icons/iconfont/material-icons.css'
+import "@picocss/pico/css/pico.min.css";
 
 const app = createApp(App)
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: () => import('./views/LoginView.vue')
+    },
+    {
+      path: '/exam/:user/:dictionary',
+      name: 'exam',
+      component: () => import('./views/ExamView.vue')
+    },
+    {
+      path: '/score/:user',
+      name: 'score',
+      component: () => import('./views/ScoreView.vue')
+    }
+  ]
+})
 
 app.use(router)
 
